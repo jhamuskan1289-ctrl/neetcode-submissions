dbb74e7d1f91685799cb933657fbed8083532c61
cattos <jhamuskan1289@gmail.com>
2026-05-25 18:29:03 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        return str(strs)
+        
+
+    def decode(self, s: str) -> List[str]:
+       return ast.literal_eval(s)
+        
+
+
+
+
